956e55e6f2bce9c1da8af0d248f426a8973d0474
Shane O'Neill <shane.oneill@kneat.org>
2019-12-06 10:57:06 +0000

    Initial commit of 01_part01.ps1 and tests.

---
+++ 2019/01_part01.Tests.ps1
@@ -0,0 +1,19 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+Describe "01_part01" {
+    It "passes the examples given in the description" -TestCases @(
+        @{Mass = 12; Result = 2}
+        @{Mass = 14; Result = 2}
+        @{Mass = 1969; Result = 654}
+        @{Mass = 100756; Result = 33583}
+    ) -Test {
+        param (
+            $Mass,
+            $Result
+        )
+        $FuelRequired = Get-FuelPerModuleMass -Mass $Mass
+        $FuelRequired.Fuel | Should -Be $Result
+    }
+}
---
+++ 2019/01_part01.ps1
@@ -0,0 +1,42 @@
+﻿function Get-FuelPerModuleMass {
+    <#
+    .SYNOPSIS
+        Gets required fuel per the module mass.
+    .DESCRIPTION
+        Fuel required to launch a given module is based on its mass.
+            Specifically, to find the fuel required for a module, take its mass, divide by three,
+            round down, and subtract 2.
+    .EXAMPLE
+        PS C:\> Get-FuelPerModuleMass -Mass 12
+
+        Mass Fuel
+        ---- ----
+          12    2
+    .EXAMPLE
+        PS C:\> Get-FuelPerModuleMass -Mass 12, 14
+
+        Mass Fuel
+        ---- ---- 
+          12    2
+          14    2
+    .INPUTS
+        [int[]]
+    .OUTPUTS
+        [PSCustomObject]
+    #>
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory,
+                   ValueFromPipeline)]
+        [int[]]$Mass
+    )
+
+    process {
+        foreach ($moduleMass in $Mass) {
+            [PSCustomObject]@{
+                Mass = $moduleMass
+                Fuel = [Math]::Floor(($moduleMass / 3)) - 2
+            }
+        }
+    }
+}
