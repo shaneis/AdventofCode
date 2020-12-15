f462de0b739705fc5dd2cc238e72a110b957004e
Shaneis <shaneoneillis@gmail.com>
2020-12-15 13:00:16 +0000

    Removed verbose messages

--- 2020/Day14/part01.ps1
+++ 2020/Day14/part01.ps1
@@ -18,7 +18,6 @@ foreach ($Instruction in $InputContent) {
     $Memory, $Value = $Instruction -split ' = '
 
     if ($Memory -eq 'mask') {
-        Write-PSFMessage -Level Host -Message ('Mask: {0}' -f $Value)
         $Mask = $value
         continue
     }
@@ -29,22 +28,13 @@ foreach ($Instruction in $InputContent) {
     
     $char_string = [Text.StringBuilder]::new()
     for ($i = -1; $i -ge (0 - $Mask.Length); $i--) {
-        Write-PSFMessage -Level Verbose -Message "Index: $i - Comparing $($ValueDecimal[$i]) against $($Mask[$i])"
         $char = switch ($Mask[$i]) {
             '0' { '0' }
             '1' { '1' }
             'X' { if ($null -ne $ValueDecimal[$i]) { $ValueDecimal[$i] } else { '0' }}
         }
-        Write-PSFMessage -Level Verbose -Message "Adding $char to $($char_string.ToString())"
         $char_string.Insert(0, $char) | Out-Null
     }
-    <#[PSCustomObject]@{
-        InputValue = -join $ValueDecimal
-        Mask = $Mask
-        OutputValue = $char_string.ToString()
-        ConvertedDecimal = [Convert]::ToInt32($char_String, 2)
-        Location = $Location.Local
-    }#>
     $Program[$Location.Local] = [Convert]::ToInt64($char_string, 2)
 }
 
