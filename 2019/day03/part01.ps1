2d295113d94bab681963e35bba2f6c38e72ec7e5
Shane O'Neill <shaneoneillis@gmail.com>
2021-11-06 15:18:54 +0000

    moved ConvertTo-WirePath to its own file

--- 2019/day03/part01.ps1
+++ 2019/day03/part01.ps1
@@ -5,6 +5,7 @@ param (
 
 #region import functions
 . /home/soneill/git/AdventofCode/2019/day03/ConvertTo-WireInstructions.ps1
+. /home/soneill/git/AdventofCode/2019/day03/ConvertTo-WirePath.ps1
 #endregion
 
 $TestLines = Get-Content -Path $Path
@@ -13,56 +14,6 @@ $Wire01, $Wire02 = $TestLines -split '\r?\n'
 
 Write-Verbose "Wire 01: [$Wire01]"
 Write-Verbose "Wire 02: [$Wire02]"
-
-function ConvertTo-WirePath {
-	[CmdletBinding()]
-	param(
-		[Parameter(
-			Mandatory,
-			ValueFromPipeline
-		)]
-		[psobject] $WireInstruction
-	)
-
-	begin {
-		$Origin = [PSCustomObject]@{
-			X = 0
-			Y = 0
-		}
-	}
-
-	end {
-		foreach ($wi in $WireInstruction) {
-			Write-Verbose "Working on: [$wi]"
-			foreach ($Position in $wi) {
-				Write-Verbose "Working on position: [$Position]"
-				foreach ($X in 0..$Position.XDelta) {
-					$IncrementX = if ($X -eq 0) {0} elseif ($X -lt 0) {-1} else {1}
-
-					$NewOrigin = [PSCustomObject] @{
-						X = $Origin.X + $IncrementX
-						Y = $Origin.Y
-					}
-					$NewOrigin
-
-					$Origin = $NewOrigin
-				}
-
-				foreach ($Y in 0..$Position.YDelta) {
-					$IncrementY = if ($Y -eq 0) {0} elseif ($Y -lt 0) {-1} else {1}
-
-					$NewOrigin = [PSCustomObject]@{
-						X = $Origin.X
-						Y = $Origin.Y + $IncrementY
-					}
-					$NewOrigin
-
-					$Origin = $NewOrigin
-				}
-			}
-		}
-	}
-}
 
 $Wire01Instructions = ConvertTo-WireInstructions -Wire $Wire01
 $Wire02Instructions = ConvertTo-WireInstructions -Wire $Wire02
