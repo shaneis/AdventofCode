f69e236dd1c720df84625f09c4eafd895aed2c41
Shane O'Neill <shaneoneillis@gmail.com>
2021-12-13 19:55:48 +0000

    Test-Row and Test-Column functions

--- 2021/day04/Test-Row.ps1
+++ 2021/day04/Test-Row.ps1
@@ -34,12 +34,15 @@ function Test-Row {
 			Select-Object -First 1 -Skip ($Row -1)
 
 		foreach ($BoardRow in $BoardRows) {
-			foreach ($n in ($BoardRow -split ' ')) {
-				if ($n -notin $CalledNumbers) {
-					$IsComplete = $false
-					break
+			$BoardNumbers = $BoardRow -split ' ' | Where-Object {$_ -as [int] -or $_ -eq '0'}
+			foreach ($n in $BoardNumbers) {
+				Write-Verbose "Row: $Row - Comparing: $n_int against $($CalledNumbers -join ',')"
+					[int] $n_int = $n
+					 if ($n_int -notin $CalledNumbers) {
+						$IsComplete = $false
+						break
+					} 
 				}
-			}
 
 			[PSCustomObject] @{
 				BoardRow = $BoardRow
