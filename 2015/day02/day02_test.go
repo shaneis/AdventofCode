3dd8b89f038ca3304968dd562a4a3e4f866aea1b
Shane O'Neill <shaneoneillis@gmail.com>
2024-11-14 07:47:38 +0000

    Advent of Code 2015. Day 02. Part 02
    Added test for getRibbonSize

--- 2015/day02/day02_test.go
+++ 2015/day02/day02_test.go
@@ -18,3 +18,20 @@ func TestGetWrappingPaperArea(t *testing.T) {
 		}
 	}
 }
+
+func TestGetRibbonSize(t *testing.T) {
+	tests := []struct {
+		input  string
+		output int
+	}{
+		{"2x3x4", 34},
+		{"1x1x10", 14},
+	}
+
+	for _, test := range tests {
+		got := getRibbonSize(test.input)
+		if got != test.input {
+			t.Errorf("Expected %d, got %d. Parameters: %s\n", test.output, got, test.input)
+		}
+	}
+}
