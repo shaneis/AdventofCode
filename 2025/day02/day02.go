fa7b700f0de80f55de30a9cd614cad2a008023ba
Shane O'Neill <shaneoneillis@gmail.com>
2025-12-05 03:18:14 +0000

    Working day 02, part 02

--- 2025/day02/day02.go
+++ 2025/day02/day02.go
@@ -31,6 +31,43 @@ func findInvalidIds(numberRange string) []int {
 	return invalids
 }
 
+func findInvalidIdsAtLeastTwice(numberRange string) []int {
+	r := strings.Split(numberRange, "-")
+	start, _ := strconv.Atoi(r[0])
+	end, _ := strconv.Atoi(r[1])
+	var invalids []int
+	// go through the ranges
+	for i := start; i <= end; i++ {
+		s := strconv.Itoa(i)
+		// split in half then smaller and smaller parts
+		for j := 1; j <= len(s); j++ {
+			if len(s)%j != 0 {
+				continue
+			}
+			var splitted []string
+			for i := 0; i <= len(s)-j; i += j {
+				splitted = append(splitted, s[i:i+j])
+			}
+			if len(splitted) < 2 {
+				continue
+			}
+			// check if all parts are equal
+			allEqual := true
+			for k := 1; k < len(splitted); k++ {
+				if splitted[k] != splitted[0] {
+					allEqual = false
+					break
+				}
+			}
+			if allEqual {
+				invalids = append(invalids, i)
+				break
+			}
+		}
+	}
+	return invalids
+}
+
 func parseInputFile(filepath string) []string {
 	file, err := os.Open(filepath)
 	if err != nil {
@@ -55,12 +92,18 @@ func main() {
 	flag.Parse()
 	lines := parseInputFile(*filepath)
 
-	var total int64 = 0
+	var totalPart01 int64 = 0
+	var totalPart02 int64 = 0
 	for _, line := range lines {
 		invalids := findInvalidIds(line)
 		for _, inval := range invalids {
-			total += int64(inval)
+			totalPart01 += int64(inval)
+		}
+		invalidsAtLeastTwice := findInvalidIdsAtLeastTwice(line)
+		for _, inval := range invalidsAtLeastTwice {
+			totalPart02 += int64(inval)
 		}
 	}
-	fmt.Printf("Day 02-i: %d\n", total)
+	fmt.Printf("Day 02-i: %d\n", totalPart01)
+	fmt.Printf("Day 02-ii: %d\n", totalPart02)
 }
--- 2025/day02/day02_test.go
+++ 2025/day02/day02_test.go
@@ -29,3 +29,34 @@ func TestFindInvalidIds(t *testing.T) {
 		}
 	}
 }
+
+func TestFindInvalidIdsAtLeastTwice(t *testing.T) {
+	tests := []struct {
+		numberRange string
+		count       int
+		invalids    []int
+	}{
+		{"11-22", 2, []int{11, 22}},
+		{"95-115", 2, []int{99, 111}},
+		{"998-1012", 2, []int{999, 1010}},
+		{"1188511880-1188511890", 1, []int{1188511885}},
+		{"222220-222224", 1, []int{222222}},
+		{"1698522-1698528", 0, []int{}},
+		{"446443-446449", 1, []int{446446}},
+		{"38593856-38593862", 1, []int{38593859}},
+		{"565653-565659", 1, []int{565656}},
+		{"824824821-824824827", 1, []int{824824824}},
+		{"2121212118-2121212124", 1, []int{2121212121}},
+	}
+	for id, test := range tests {
+		invalids := findInvalidIdsAtLeastTwice(test.numberRange)
+		if len(invalids) != test.count {
+			t.Errorf("Test %d: Expected %d invalid IDs, got %d", id, test.count, len(invalids))
+		}
+		for i, inval := range invalids {
+			if inval != test.invalids[i] {
+				t.Errorf("Test %d: Expected invalid ID %d at index %d, got %d", id, test.invalids[i], i, inval)
+			}
+		}
+	}
+}
