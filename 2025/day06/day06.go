e4c28e6962af6a62b1734589a47c69bb96c71e17
Shane O'Neill <shaneoneillis@gmail.com>
2025-12-21 15:20:45 +0000

    Working Day 06, part 02

--- 2025/day06/day06.go
+++ 2025/day06/day06.go
@@ -26,6 +26,41 @@ func getFileInput(filePath string) []string {
 	return fileContents
 }
 
+func getFileInputAsColumns(filePath string) [][]string {
+	fileContents := getFileInput(filePath)
+	var results []string
+	var nr [][]string
+	keepGoing := true
+	i := 0
+	var thisResult strings.Builder
+	for keepGoing {
+		columnBreak := true
+		for _, line := range fileContents[:len(fileContents)-1] {
+			if columnBreak {
+				thisResult = strings.Builder{}
+			}
+			keepGoing = false
+			if i >= len(line) {
+				// deal with some lines longer than others
+				continue
+			} else {
+				if line[i] != byte(' ') {
+					thisResult.WriteByte(line[i])
+					columnBreak = false
+				}
+				keepGoing = true
+			}
+		}
+		if !columnBreak {
+			results = append(results, thisResult.String())
+		} else {
+			nr = append(nr, results)
+			results = []string{}
+		}
+		i++
+	}
+	return nr
+}
 func solvePart1(numbers []string, actions []string) []int {
 	numAnswers := len(actions)
 	results := make([]int, numAnswers)
@@ -54,6 +89,38 @@ func solvePart1(numbers []string, actions []string) []int {
 	return results
 }
 
+func solvePart2(numbers [][]string, actions []string) []int {
+	numberActions := len(actions)
+	results := make([]int, numberActions)
+
+	for i := 0; i < len(numbers); i++ {
+		line := numbers[i]
+
+		first := true
+		for j := 0; j < len(line); j++ {
+			thisNum, _ := strconv.Atoi(line[j])
+			switch actions[i] {
+			case "+":
+				if first {
+					results[i] = thisNum
+					first = false
+				} else {
+					results[i] += thisNum
+				}
+			case "*":
+				if first {
+					results[i] = thisNum
+					first = false
+				} else {
+					results[i] *= thisNum
+				}
+			}
+		}
+	}
+
+	return results
+}
+
 func main() {
 	file := flag.String("file", "sample_input_01.txt", "Input file path")
 	flag.Parse()
@@ -71,4 +138,12 @@ func main() {
 		day06Part01Results += res
 	}
 	fmt.Println("Day 06 Part 01 results:", day06Part01Results)
+
+	secondFileContents := getFileInputAsColumns(*file)
+	secondResults := solvePart2(secondFileContents, strings.Fields(actions))
+	day06Part02Results := 0
+	for _, res := range secondResults {
+		day06Part02Results += res
+	}
+	fmt.Println("Day 06 Part 02 results:", day06Part02Results)
 }
